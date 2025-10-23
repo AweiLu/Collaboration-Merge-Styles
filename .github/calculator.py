6d69a6bb987fc185fe188abb11e39a8003bc7d82
awei <b11209047@mail.ntust.edu.tw>
2025-10-23 14:17:49 +0800

    commit

---
+++ .github/calculator.py
@@ -0,0 +1,18 @@
+# calculator.py
+def add(a, b):
+    """將兩個數字相加"""
+    return a + b
+
+def subtract(a, b):
+    """將兩個數字相減"""
+    return a - b
+
+def multiply(a, b):
+    """將兩個數字相乘"""
+    return a * b
+
+def divide(a, b):
+    """將兩個數字相除"""
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
