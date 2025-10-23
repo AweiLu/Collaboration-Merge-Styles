96cc805232ec944e779cbe3c1a1446600665517f
awei <b11209047@mail.ntust.edu.tw>
2025-10-23 14:35:36 +0800

    CI test

--- .github/calculator.py
+++ .github/calculator.py
@@ -3,13 +3,16 @@ def add(a, b):
     """將兩個數字相加"""
     return a + b
 
+
 def subtract(a, b):
     """將兩個數字相減"""
     return a - b
 
+
 def multiply(a, b):
     """將兩個數字相乘"""
     return a * b
+
 
 def divide(a, b):
     """將兩個數字相除"""
